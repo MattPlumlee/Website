90eb3ece7d57c70fc2221c284ff5811aa41fa7b6
Matt P <mplumlee.data@gmail.com>
2020-08-04 11:49:35 -0500

    Initial files

---
+++ header.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+
+
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title>Matthew Plumlee - Northwestern </title>
+
+  <!-- Included CSS Files (Uncompressed) -->
+  <!--
+  <link rel="stylesheet" href="stylesheets/foundation.css">
+  -->
+
+  <!-- Included CSS Files (Compressed) -->
+  <link rel="stylesheet" href="stylesheets/foundation.css">
+  <link rel="stylesheet" href="stylesheets/app.css">
+
+  <script src="javascripts/modernizr.foundation.js"></script>
+</head>
+<body>
+<div id="container">
+  <div class="row"><br>
+    <div class="three columns offset-by-two">
+      <img class="pic" src="images/matthew_plumlee.jpg">
+    </div>
+
+     <div class="six columns offset-by-one">
+        <h2>Matthew Plumlee</h2>
+        <p class="header"><b>Assistant Professor </b><br>
+        Industrial Engineering and Management Sciences<br>
+          Northwestern University<br>
+
+<script type="text/javascript" language="javascript">
+<!--
+// Email obfuscator script 2.1 by Tim Williams, University of Arizona
+// Random encryption key feature by Andrew Moulden, Site Engineering Ltd
+// This code is freeware provided these four comment lines remain intact
+// A wizard to generate this code is at http://www.jottings.com/obfuscator/
+{ coded = "HbLAHLxx@7ViU2dxgUxi7.xjA"
+  key = "4isGXxPUywvHJNMq9b5fQISD2lWg8ZcuoBdn0YrFCRe1tKEkmTLa63pOAVzj7h"
+  shift=coded.length
+  link=""
+  for (i=0; i<coded.length; i++) {
+    if (key.indexOf(coded.charAt(i))==-1) {
+      ltr = coded.charAt(i)
+      link += (ltr)
+    }
+    else {
+      ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+      link += (key.charAt(ltr))
+    }
+  }
+document.write("<h4> <a href='mailto:"+link+"'>EMAIL</a> </h4>")
+}
+//-->
+</script><noscript>Sorry, you need Javascript on to email me.</noscript>
+
+
+    </div>
+  </div>
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+?>
+
+
+<?php include 'header.php';?>
+
+  <div class="row"><br>
+    <div class="ten columns offset-by-one">
+      <div class="panel">
+
+        <p>I work and teach on the interface between data, technology and science.  
+My research covers experimental and computational methods for combining observations and simulation models for descriptive, predictive and prescriptive purposes in large-data environments. 
+ The major tools used are machine learning and statistical analysis, large-scale optimization and designed experimentation including sequential design.</p>
+
+        <h3>education</h3>
+
+        <p>Doctor of Philosophy, Industrial Engineering, Georgia Institute of Technology, 2015. <br>
+          Master of Science, Statistics, Georgia Institute of Technology, 2011. <br>
+          Bachelor of Science, Mechanical Engineering, Purdue University, 2010.</p>
+
+
+
+              <h3>research   <a href="http://scholar.google.com/citations?hl=en&user=sSQeiUwAAAAJ" >[google scholar page] </a>  </h3>
+
+              <ul style="list-style-type:none;">
+                <li><h4>journal publications </h4> </li> <ul style="list-style-type:none;">
+                <li> C-L. Sung, W. Wang, M. Plumlee, B. Haaland. "Multi-resolution functional ANOVA for large-Scale, many-input computer experiments."  Journal of the American Statistical Association, to appear. </li>
+        	      <li> M. Plumlee.  "Calibration with confidence and consistency."   Journal of the Royal Statistical Society: Series B, vol. 81, no. 3, pp. 519-545, 2019. </li>
+                <li> M. Plumlee, V.R. Joseph.  "Orthogonal Gaussian process models."   Statistical Sinica, vol. 28, no. 2, pp. 601-619, 2018. </li>
+                <li> M. Plumlee.  "Bayesian calibration of inexact computer models." Journal of the American Statistical Association, vol. 112, no. 519, pp. 1274-1285, 2017. </li>
+                <li> M. Plumlee, D. Apley.  "Lifted Brownian kriging models."  Technometrics, vol. 59, no. 2, pp. 165-177, 2017. </li>
+                <li>  M. Plumlee,  V.R. Joseph, H. Yang.  "Calibrating functional parameters of the ion channels of cardiac cells."  Journal of the American Statistical Association, vol. 111, no. 514, pp. 500-509, 2016. </li>
+                <li> M. Plumlee,  "Fast prediction of deterministic functions using sparse grid experimental designs." Journal of the American Statistical Association, vol. 109, no. 508, pp. 1581-1591, 2014. </li>
+                  &nbsp;&nbsp;  <a href="http://www.mathworks.com/matlabcentral/fileexchange/45668-sparse-grid-designs" rel="nofollow" onClick="trackOutboundLink(this, 'SG', 'SG Code Download'); return false;">[matlab code]</a> </li><br>
+                <li>M. Plumlee, R. Tuo, "Building accurate emulators for stochastic simulations via quantile kriging." Technometrics}, vol. 56, no. 4, pp. 466-473, 2014. </li> <br> </ul>
+              <li><h4>conference publications</h4> </li> <ul style="list-style-type:none;">
+                 <li> M. Plumlee, B. Nelson Plausible optima,  Proceedings of the Winter Simulation Conference, pp. 1981-1992, 2018. </li>
+                 <li> C. Erickson, B. Ankenman, M. Plumlee, S. Sanchez. Gradient based criteria for sequential design,   Proceedings of the Winter Simulation Conference, pp. 467-478, 2018. </li>
+                 <li> B. Liu, M. Plumlee, E. Byon. Data-driven parameter calibration in wake models,   Wind Energy Symposium, 2018. </li>
+                 <li> H. Lam, X. Zhang, M. Plumlee Improving prediction from stochastic simulation via model discrepancy learning,  Proceedings of the Winter Simulation Conference,  pp. 1808-1819, 2017. </li>
+                 <li> M. Plumlee, H. Lam.  Learning simulation model discrepancy. Proceedings of the  Winter Simulation Conference, pp. 413-424, 2016. </li>
+                 <li> R. Langan, R. Archibald, M. Plumlee, S. Mahajan, D. Ricciuto, C-E. Yang, R. Mei, J. Mao, X. Shi, J. Fu, Stochastic parameterization to represent variability and extremes in climate modeling. Procedia Computer Science, pp. 1146-1155, 2014. </li>
+              </ul>
+            </ul>
+                  <h3>service</h3>
+                                <ul style="list-style-type:none;">
+                    <li>  Assoicate Editor, Technometrics, 2019-  </li>
+                    <li> Organizing Committee, Statistical perspectives on uncertainty quantification, 2017
+                    <li>  Best student paper competition organizer, INFORMS Quality, Reliability and Statistics Section, 2018  </li>
+                    <li>  Advisory council, INFORMS Quality, Reliability and Statistics Section, 2016-2018  </li>
+                    <li>  Treasurer, ASA Quality and Productivity Section, 2016-2018  </li>
+                    </ul>
+                      <h3>advising</h3>
+                                    <ul style="list-style-type:none;">
+                      <li> David Eckman  (with Barry Nelson), Postdoc 2019-   </li>
+                      <li> Moses Chan, PhD expected 2022   </li>
+                      <li> Collin Erickson  (with Bruce Ankenman), PhD awarded 2019   </li>
+                      <li> Wenbo Sun (with Judy Jin), PhD awarded 2018  </li>
+                      </ul>
+                      <h3>teaching</h3>
+                                    <ul style="list-style-type:none;">
+<li> IEMS 401, Applied Mathematical Statistics, Northwestern University: Fall 2018 </li>
+<li>IEMS 303,  Statistics, Northwestern University: Winter 2019, Fall 2018, Winter 2018, Fall 2017  </li>
+<li>IOE 465, Design and Analysis of Experiments, University of Michigan: Winter 2017, Winter 2016  </li>
+<li>IOE 591, Bayesian Data Analysis, University of Michigan: Fall 2016. </li>
+<li>ISYE 3039, Methods for Quality Improvement, Georgia Institute of Technology: Fall 2014.  </li>
+</ul>
+<h3> funding </h3>
+<ul style="list-style-type:none;">
+<li>
+EAGER/Collaborative Research: Exploring the Theoretical Framework of Engineering Knowledge Transfer in Cybermanufacturing Systems. National Science Foundation, CMMI 1833195 </li>
+</ul>
+
+<?php include 'footer.php';?>
