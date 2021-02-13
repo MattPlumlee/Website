3d3e2716e488fd9a32fe1d544b99c6e37c939031
Matt P <mplumlee.data@gmail.com>
2021-02-13 17:03:21 -0600

    more head adj

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Matthew Plumlee - Northwestern </title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css"  href="stylesheets/bibtexbrowser.css">
   <link rel="stylesheet" type="text/css"  href="stylesheets/foundation.css">
   <link rel="stylesheet" type="text/css"  href="stylesheets/app.css">
