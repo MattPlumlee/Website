e5ffbb1daef0d7a84806fcbbea43bf3a153b376a
Matt P <mplumlee.data@gmail.com>
2021-02-13 17:46:53 -0600

    Setting a better max width

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 
 
 <body>
-<div class="container-fluid" max-width:"900px">
+<div class="container-lg">
   <div class="row"><br>
     <div class="col-sm">
       <img class="img-responsive" src="images/matthew_plumlee.jpg"  max-width="400px" width="300px">
