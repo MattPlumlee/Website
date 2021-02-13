82704ca382e41991275271524c52c9448246c9ae
Matt P <mplumlee.data@gmail.com>
2021-02-13 15:06:13 -0600

    displaying important publications

--- index.php
+++ index.php
@@ -20,50 +20,38 @@ My research covers experimental and computational methods for combining observat
           Bachelor of Science, Mechanical Engineering, Purdue University, 2010.</p>
 
 
+          <?php
+          $_GET['library']=1;
+          require_once('bibtexbrowserv1.php');
+          global $db;
+          $db = new BibDataBase();
+          $db->load('plumleebib.bib');
+          ?>
 
-              <h3>research   <a href="http://scholar.google.com/citations?hl=en&user=sSQeiUwAAAAJ" >[google scholar page] </a>  </h3>
+<h3>research   <a href="http://scholar.google.com/citations?hl=en&user=sSQeiUwAAAAJ" >[google scholar page] </a>  </h3>
 
-              <ul style="list-style-type:none;">
-                <li><h4>journal publications </h4> </li> <ul style="list-style-type:none;">
-                <li> M. Plumlee, T. G. Asher, W. Chang, M. V. Bilskie. "High-fidelity hurricane surge forecasting using emulation and sequential experiments ."  Annals of Applied Statistics, to appear. </li>
-                <li> M. Plumlee, C. B. Erickson, B. E. Ankenman, E. Lawrence. "Composite grid designs for adaptive computer experiments with fast inference ."  Biometrika, to appear. </li>
-                    &nbsp;&nbsp;  <a href="https://cran.r-project.org/web/packages/CGGP/index.html">[R package] </a>  <a href="https://academic.oup.com/biomet/advance-article/doi/10.1093/biomet/asaa084/5923289?guestAccessKey=50e6d7f6-c122-4a93-a1ae-42bb3143ef59">[Journal] </a> <br>
-                <li> C-L. Sung, W. Wang, M. Plumlee, B. Haaland. "Multi-resolution functional ANOVA for large-Scale, many-input computer experiments."  Journal of the American Statistical Association, vol. 115, no. 530, pp. 908-919, 2020. </li>
-                    &nbsp;&nbsp;  <a href="https://cran.r-project.org/web/packages/MRFA/index.html">[R package]</a> <br>
-                <li>A. van Beek, S. Tao, M., D. Apley, W. Chen "Integration of Normative Decision-Making and Batch Sampling for Global Metamodeling." Journal of Mechanical Design, vol 142, no. 3, 2020. </li>
-        	      <li> M. Plumlee.  "Calibration with confidence and consistency."   Journal of the Royal Statistical Society: Series B, vol. 81, no. 3, pp. 519-545, 2019. </li>
-                <li> M. Plumlee, V.R. Joseph.  "Orthogonal Gaussian process models."   Statistical Sinica, vol. 28, no. 2, pp. 601-619, 2018. </li>
-                <li> M. Plumlee.  "Bayesian calibration of inexact computer models." Journal of the American Statistical Association, vol. 112, no. 519, pp. 1274-1285, 2017. </li>
-                <li> M. Plumlee, D. Apley.  "Lifted Brownian kriging models."  Technometrics, vol. 59, no. 2, pp. 165-177, 2017. </li>
-                <li>  M. Plumlee,  V.R. Joseph, H. Yang.  "Calibrating functional parameters of the ion channels of cardiac cells."  Journal of the American Statistical Association, vol. 111, no. 514, pp. 500-509, 2016. </li>
-                <li> M. Plumlee,  "Fast prediction of deterministic functions using sparse grid experimental designs." Journal of the American Statistical Association, vol. 109, no. 508, pp. 1581-1591, 2014. </li>
-                  &nbsp;&nbsp;  <a href="http://www.mathworks.com/matlabcentral/fileexchange/45668-sparse-grid-designs">[matlab code]</a> <br>
-                <li>M. Plumlee, R. Tuo, "Building accurate emulators for stochastic simulations via quantile kriging." Technometrics}, vol. 56, no. 4, pp. 466-473, 2014. </li> <br> </ul>
-              <li><h4>conference publications</h4> </li> <ul style="list-style-type:none;">
-                <li> D. Eckman, M. Plumlee, B. Nelson.  Revisiting Subset Selection, upcoming Proceedings of the Winter Simulation Conference </li>
-                 <li> M. Plumlee, B. Nelson. Plausible optima,  Proceedings of the Winter Simulation Conference, pp. 1981-1992, 2018. </li>
-                 <li> C. Erickson, B. Ankenman, M. Plumlee, S. Sanchez. Gradient based criteria for sequential design,   Proceedings of the Winter Simulation Conference, pp. 467-478, 2018. </li>
-                 <li> B. Liu, M. Plumlee, E. Byon. Data-driven parameter calibration in wake models,   Wind Energy Symposium, 2018. </li>
-                 <li> H. Lam, X. Zhang, M. Plumlee Improving prediction from stochastic simulation via model discrepancy learning,  Proceedings of the Winter Simulation Conference,  pp. 1808-1819, 2017. </li>
-                 <li> M. Plumlee, H. Lam.  Learning simulation model discrepancy. Proceedings of the  Winter Simulation Conference, pp. 413-424, 2016. </li>
-                 <li> R. Langan, R. Archibald, M. Plumlee, S. Mahajan, D. Ricciuto, C-E. Yang, R. Mei, J. Mao, X. Shi, J. Fu, Stochastic parameterization to represent variability and extremes in climate modeling. Procedia Computer Science, pp. 1146-1155, 2014. </li>
-              </ul>
-            </ul>
-                  <h3>service</h3>
-                                <ul style="list-style-type:none;">
-                    <li>  Associate Editor, Technometrics, 2019-  </li>
-                    <li> Organizing Committee, Statistical perspectives on uncertainty quantification, 2017
-                    <li>  Best student paper competition organizer, INFORMS Quality, Reliability and Statistics Section, 2018  </li>
-                    <li>  Advisory council, INFORMS Quality, Reliability and Statistics Section, 2016-2018  </li>
-                    <li>  Treasurer, ASA Quality and Productivity Section, 2016-2018  </li>
-                    </ul>
-                      <h3>advising</h3>
-                                    <ul style="list-style-type:none;">
-                      <li> David Eckman  (with Barry Nelson), Postdoc 2019-   </li>
-                      <li> Moses Chan, PhD expected 2022   </li>
-                      <li> Collin Erickson  (with Bruce Ankenman), PhD awarded 2019   </li>
-                      <li> Wenbo Sun (with Judy Jin), PhD awarded 2018  </li>
-                      </ul>
+<ul style="list-style-type:none;">
+  <li><h4>representative method publications </h4> </li> <ul style="list-style-type:none;">
+    <? php
+    $query = array('highlightmethod'=>'1');
+    $entries=$db->multisearch($query);
+    uasort($entries, 'compare_bib_entries');
+
+    foreach ($entries as $bibentry) {
+      echo $bibentry->toHTML()."<br/>";
+    }
+    ?>
+    <li><h4>representative application publications </h4> </li> <ul style="list-style-type:none;">
+      <? php
+      $query = array('highlightappl'=>'1');
+      $entries=$db->multisearch($query);
+      uasort($entries, 'compare_bib_entries');
+
+      foreach ($entries as $bibentry) {
+        echo $bibentry->toHTML()."<br/>";
+      }
+      ?>
+
                       <h3>teaching</h3>
                                     <ul style="list-style-type:none;">
 <li> IEMS 490, Principals of Uncertainty Quantification, Northwestern University:  Winter 2021 </li>
@@ -85,20 +73,5 @@ My research covers experimental and computational methods for combining observat
  &nbsp;&nbsp;  Local PI,  06/2017-06/2018
 </ul>
 
-<?php
-$_GET['library']=1;
-require_once('bibtexbrowserv1.php');
-global $db;
-$db = new BibDataBase();
-$db->load('plumleebib.bib');
-
-$query = array('highlight'=>'1');
-$entries=$db->multisearch($query);
-uasort($entries, 'compare_bib_entries');
-
-foreach ($entries as $bibentry) {
-  echo $bibentry->toHTML()."<br/>";
-}
-?>
 
 <?php include 'footer.php';?>
