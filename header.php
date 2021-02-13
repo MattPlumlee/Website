7d26ddd4a9422dacc005eac94efa380d0ca345c5
Matt P <mplumlee.data@gmail.com>
2021-02-13 16:31:43 -0600

    another attmpt

--- header.php
+++ header.php
@@ -68,17 +68,24 @@
 </div>
 
 
-<Nav>
-  <NavItem active>
-    <a>One</a>
-  </NavItem>
-  <NavItem>
-    <a>Two</a>
-  </NavItem>
-  <NavItem>
-    <a>Three</a>
-  </NavItem>
-  <NavItem className="button hollow primary">
-    <a>Logout</a>
-  </NavItem>
-</Nav>
+
+    <div class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+
+    <a class="navbar-brand" href="/">BAND Framework Project</a>
+	</div>
+	<div class="collapse navbar-collapse" id="navbar-collapse-1">
+	  <ul class="nav navbar-nav navbar-right">
+		<li><a href="/">Home</a></li>
+		<li><a href="/team">Team</a></li>
+	  </ul>
+	</div>
+  </div>
+</div>
