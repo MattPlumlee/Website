a1ede986596b85bb8b030bff01def7f1d0dfbdf5
Matt P <mplumlee.data@gmail.com>
2021-02-13 12:59:15 -0600

    moar bibtex attempts

--- index.php
+++ index.php
@@ -88,17 +88,10 @@ My research covers experimental and computational methods for combining observat
 
 <?php
 $_GET['library']=1;
-define('BIBTEXBROWSER_BIBTEX_LINKS',false); // no [bibtex] link by default
 require_once('bibtexbrowser.php');
 global $db;
 $db = new BibDataBase();
-$db->load('biblio.bib');
-
-// printing all 2014 entries
-// can also be $query = array('year'=>'.*');
-$query = array('year'=>'2014');
-$entries=$db->multisearch($query);
-uasort($entries, 'compare_bib_entries');
+$db->load('plumleebib.bib');
 
 foreach ($entries as $bibentry) {
   echo $bibentry->toHTML()."<br/>";
@@ -109,6 +102,6 @@ foreach ($entries as $bibentry) {
 $_GET['bib']='csgroup2008.bib';
 $_GET['all']=1;
 include( 'bibtexbrowser.php' );
-?> 
+?>
 
 <?php include 'footer.php';?>
