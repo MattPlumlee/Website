6ee0b5656168f11d921f54a484d7d288e073c389
Matt P <mplumlee.data@gmail.com>
2021-02-14 00:16:12 -0600

    Fixing up the navbar

--- header.php
+++ header.php
@@ -60,34 +60,11 @@
 </div>
 
 
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-<br>
-
 <nav class="navbar navbar-expand-ls bg-light justify-content-center bg-transparent">
-  <!-- Brand -->
-  <!-- Toggler/collapsibe Button -->
+  <a class="navbar-brand" href="#"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
-
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
@@ -105,3 +82,4 @@
     </ul>
   </div>
 </nav>
+<br>
