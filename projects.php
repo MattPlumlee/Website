16e851656f70a4272d31b875002a840c129c04f1
Matt P <mplumlee.data@gmail.com>
2021-02-14 00:34:21 -0600

    The beginning of projects

--- projects.php
+++ projects.php
@@ -1,1 +1,28 @@
-To be filled in later!
+<?php
+session_start();
+?>
+
+<?php include 'header.php';?>
+
+
+<h3>projects</h3>
+
+These are significant, and often funded projects of current interest.
+
+
+<ul class="list-group">
+<li class="list-group-item">
+<img class="img-responsive" src="https://bandframework.github.io/images/logopic/BAND_logo_v2.png"  max-width="400px" width="300px">
+The Bayesian Analysis of Nuclear Dynamics (BAND) Framework will use advanced statistical methods to produce forecasts for as-yet-unexplored situations that combine nuclear-physics models in an optimal way.
+<br>
+<a href="https://bandframework.github.io/"> Get on the BAND wagon </a>
+</li>
+<ul>
+
+
+
+
+
+
+
+<?php include 'footer.php';?>
