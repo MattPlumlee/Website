53dced9041f65e51f2e03f22933f2ddd5c32dd37
Matt P <mplumlee.data@gmail.com>
2021-02-13 18:20:20 -0600

    typo fix

--- header.php
+++ header.php
@@ -61,14 +61,14 @@
 </div>
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-transparent>
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="papers.php">Papers</a>
+        <a class="nav-link" href="/papers.php">Papers</a>
       </li>
     </ul>
   </div>
