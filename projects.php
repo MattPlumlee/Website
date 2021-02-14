271b3935035a56b789b902b0568f7dd5fa13a1f6
Matt P <mplumlee.data@gmail.com>
2021-02-14 01:08:51 -0600

    trying divs

--- projects.php
+++ projects.php
@@ -11,17 +11,20 @@ This a non-exhausitive, often intertwined collection of projects of current inte
 
 <ul class="list-group">
 <li class="list-group-item">
-  <p4> <a href="https://surmise.readthedocs.io/en/latest/"> surmise </p4>
+<div class="col-sm">
+<p4> <a href="https://surmise.readthedocs.io/en/latest/"> surmise</a>   </p4>
 <img class="img-responsive" src="https://avatars.githubusercontent.com/u/77858356?s=200&v=4"  max-width="200px" width="100px"> <br>
+</div>
+<div class="col-sm">
 Are you sick of redesigning interfaces every time you write code for a new emulator?  The surmise seeks to solve
 that problem by automating the interface between surrogates, or emulators, and calibration, uncetainity quanitficaiton,
 and hopefully more to come.  The goal of this project is a one-stop-shop for
 researchers interested in using emulation and calibration tools and researchers
 who design emulation and calibration tools.
-<br>
-
+</div>
 </li>
 <li class="list-group-item">
+
 <img class="img-responsive" src="images/indexsetillus.png"  max-width="400px" width="300px"> <br>
 Scaling up Gaussian processes have typically meant approximations.  Not anymore!
 This project is about scaling Gaussian process inference into very high demensions
@@ -29,6 +32,7 @@ using grid-based structures.  These are much more flexible compared to simple
 Cartesian products.
 <br>
 </li>
+
 <li class="list-group-item">
 <p4> <a href="https://bandframework.github.io/"> Get on the BAND wagon </a>   </p4>
 <img class="img-responsive" src="https://bandframework.github.io/images/logopic/BAND_logo_v2.png"  max-width="400px" width="300px"> <br>
