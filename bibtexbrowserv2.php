588553609ff8b9c3a1507ae2aed0b13456abc97e
Matt P <mplumlee.data@gmail.com>
2021-04-02 14:48:35 -0500

    css fix

--- bibtexbrowserv2.php
+++ bibtexbrowserv2.php
@@ -4112,12 +4112,12 @@ dd {
 .purebibtex {
   font-family: monospace;
   font-size: small;
-  border: 1px solid #DDDDDD;
+  /* border: 1px solid #DDDDDD; */
   /* background: none repeat scroll 0 0 #F5F5F5; */
   padding:10px;
 
   /* overflow:auto; */
-  width:600px;
+  /* width:600px; */
 
   clear:both;
 }
