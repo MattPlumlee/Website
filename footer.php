ce697348ffe015d7b97a828d426fe725b33b583c
Matt P <mplumlee.data@gmail.com>
2021-04-22 18:55:24 -0500

    footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
  <div class="row">
   <div class="ten columns centered">
     <hr>
-    <small style="float:right"><em>last update: Feburary, 2020
+    <small style="float:right"><em>last update: April, 2021
   </div>
 </div>
 
